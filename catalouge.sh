19e009e0451902d06d3029e56a6f187439b3137f
Arunponugotii <arunponugoti2565@gmail.com>
2024-01-01 22:44:59 +0530

    roboshop script

--- catalouge.sh
+++ catalouge.sh
@@ -24,6 +24,9 @@ VALIDATE(){
         echo -e "$2 .... $G SUCCESS $N"
     fi
 }
+
+yum install -y gcc-c++ make 
+
 curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash - &>>$LOGFILE
 #curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
 yum install nodejs -y &>>$LOGFILE
